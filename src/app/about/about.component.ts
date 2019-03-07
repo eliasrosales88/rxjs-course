99a0e7d90a5fbef364fa8791350716b771683bfe
Elias Rosales <eliasrosales88@gmail.com>
2019-03-07 23:53:29 +0100

    adding first observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, noop } from 'rxjs';
+
 
 @Component({
   selector: 'about',
@@ -10,6 +12,26 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const http$ = Observable.create(observer => {
+      fetch('/api/courses')
+          .then(response => {
+              return response.json();
+          })
+          .then(body => {
+              observer.next(body);
+              observer.complete();
+          })
+          .catch(err => {
+              observer.error(err);
+          })
+  });
+  
+  http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+  
+  )
   }
 
 }
